c6c0171d6bc768bd24b9ce065e765246b02f9e48
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-07-28 11:14:38 -0700

    fixed server connection

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class Category extends Model {}
 
 Category.init(
     {
