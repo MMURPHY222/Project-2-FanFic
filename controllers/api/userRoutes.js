23bdb9f036b581c25f5346cd7cbcb93aeed3c83b
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-29 13:24:50 -0400

    Made logout post route in userroutes that checks if the user is logged in and if so destroys the session to log the user out; otherwise the route yields a 404 error

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -6,5 +6,7 @@ router.post('/login', (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-
+    req.session.logged_in 
+    ? req.session.destroy(() => res.status(204).end())
+    : res.status(404).end();
 });
