0926dbc9bf23930ad66a956fa07580f67ad10f6c
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-29 11:33:13 -0400

    Added stories route to index js in api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
+const storyRoutes = require('./storyRoutes');
 
+router.use('/stories', storyRoutes);
 router.use('/categories', categoryRoutes);
 
 module.exports = router;
