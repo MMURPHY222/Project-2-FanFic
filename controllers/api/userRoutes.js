9e61ca3ccae7cc4b0716cee21fc017765df3b2d0
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-29 13:35:16 -0400

    Added password check that is based off of the target user specified by the entered email

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,14 +4,22 @@ const { User } = require('../../models');
 router.post('/login', (req, res) => {
     try {
 
-        const userData = await User.findOne({ where: { email: req.body.email } });
+        const userData = await User.findOne({ where: { email: req.body.email } }); // Searches for user by target email
+        const validPassword = await userData.checkPassword(req.body.password); // Pass check dependent on user data
 
         if(!userData) {
             res
             .status(400)
             .json({ message: 'Incorrect email or password, please try again' });
             return;
-        } 
+        }
+
+        if (!validPassword) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
 
     } catch (err) {
         res.status(400).json(err);
