db7c80932ccd2b3592ce95ec504aa457f4b5250f
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-29 13:27:40 -0400

    Added try catch to login with bad request response in catch block

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,11 +2,15 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.post('/login', (req, res) => {
+    try {
 
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.post('/logout', (req, res) => {
-    req.session.logged_in 
-    ? req.session.destroy(() => res.status(204).end())
-    : res.status(404).end();
+    req.session.logged_in // Is the user logged in?
+    ? req.session.destroy(() => res.status(204).end()) // If so, destroy the session to log them out
+    : res.status(404).end(); // Otherwise, lead the user to a 404 page upon trying to access this route
 });
