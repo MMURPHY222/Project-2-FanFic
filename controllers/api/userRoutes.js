4edd7bf7dea6b5b003964a830b1bb4f1f25c7f26
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-29 13:32:11 -0400

    Added userData check that sees if userData enetered matches any user in db with the same email and returns a bad request if not

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,6 +4,15 @@ const { User } = require('../../models');
 router.post('/login', (req, res) => {
     try {
 
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if(!userData) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        } 
+
     } catch (err) {
         res.status(400).json(err);
     }
