2f164cc65172526d050a0d83fef216bc8cc472ec
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-29 13:20:58 -0400

    Added empty post routes for login and logout in useroutes in api

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,2 +1,10 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+
+router.post('/login', (req, res) => {
+
+});
+
+router.post('/logout', (req, res) => {
+
+});
