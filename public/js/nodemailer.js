7e8052d47b2e4f24953a4060b6586397280b07a7
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-30 14:37:47 -0400

    Added options block with from and to emails and the subject and text of a test email

--- public/js/nodemailer.js
+++ public/js/nodemailer.js
@@ -4,7 +4,14 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const transporter = nodemailer.createTransport({
     service: "hotmail",
     auth: {
-        user: "brooks.gunn@vikings.berry.edu",
+        user: process.env.EM_EMAIL,
         pass: process.env.EM_PASS
     }
 });
+
+const options = {
+    from: process.env.EM_EMAIL,
+    to: process.env.TEST_EMAIL,
+    subject: "This is a test email from Node JS.",
+    text: "Hopefully this works!"
+}
