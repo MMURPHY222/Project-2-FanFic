50e74756cb1ba37fc2d979c8e620fcb742bd1fdd
Nicholas Perez <nicholas.perez290@gmail.com>
2021-07-30 10:18:40 -0400

    mostly finished view stories page

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   try {
     // const categoryData = await Category.findAll();
     // res.status(200).json(categoryData);
-    res.render('write-story', {variable: true});
+    res.render('view-stories', {variable: true});
   } catch (err) {
     res.status(500).json(err);
   }
