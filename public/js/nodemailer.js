eeded2e3dd1839c14b1b867403cdb9dcbd7465fb
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-30 14:28:33 -0400

    Added email service and authentication block to transporter

--- public/js/nodemailer.js
+++ public/js/nodemailer.js
@@ -1,5 +1,10 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const transporter = nodemailer.createTransport({
-
-})
+    service: "hotmail",
+    auth: {
+        user: "brooks.gunn@vikings.berry.edu",
+        pass: process.env.EM_PASS
+    }
+});
