4204eca7e73f88ddaa1dcc098df33747bcce0888
Nicholas Perez <nicholas.perez290@gmail.com>
2021-07-31 20:19:22 -0400

    stories display in viewstories, and single story view background is now static, home in navbar works

--- public/js/backgroundControl.js
+++ public/js/backgroundControl.js
@@ -1,19 +1,30 @@
 let toggle = document.querySelector("#toggle");
-let background = document.querySelector('#background');
-let nav = document.querySelector('#navId');
-console.log(html)
-const backgrounds = ['https://wallpapercave.com/wp/QGuGTRV.jpg', 'https://wallpaperaccess.com/full/1141048.jpg','https://wallpapercave.com/wp/eN43jqu.jpg']
+let background = document.querySelector("#background");
+let nav = document.querySelector("#navId");
+console.log('working')
+const backgrounds = [
+  "https://wallpapercave.com/wp/QGuGTRV.jpg",
+  "https://wallpaperaccess.com/full/1141048.jpg",
+  "https://wallpapercave.com/wp/eN43jqu.jpg",
+];
 // let isLight = true;
-document.body.style.backgroundImage = `url(${backgrounds[1]})`
-document.body.style.backgroundSize = 'cover'
-let i = 1
-if (window.location.pathname === '/profile' || window.location.pathname === '/story' || window.location.pathname === '/stories' || window.location.pathname === '/writestory'){
-toggle.addEventListener('click', (event) => {
-    document.body.style.backgroundImage = `url(${backgrounds[i]})`
-    if(i === (backgrounds.length - 1)) {
-        return i = 0;
-    }else {
-        return i++;
+document.body.style.backgroundImage = `url(${backgrounds[1]})`;
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundAttachment = "fixed";
+let i = 1;
+if (
+  window.location.pathname === "/profile" ||
+  window.location.pathname === "/story" ||
+  window.location.pathname === "/stories" ||
+  window.location.pathname === "/writestory" ||
+  window.location.pathname.includes("/story")
+) {
+  toggle.addEventListener("click", (event) => {
+    document.body.style.backgroundImage = `url(${backgrounds[i]})`;
+    if (i === backgrounds.length - 1) {
+      return (i = 0);
+    } else {
+      return i++;
     }
-})
+  });
 }
