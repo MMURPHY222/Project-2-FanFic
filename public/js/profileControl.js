200ab2bbc76700c5638ebf51dbd0b5d5fc272370
Nicholas Perez <nicholas.perez290@gmail.com>
2021-08-02 13:49:51 -0400

    fix slight error in profile pics

--- public/js/profileControl.js
+++ public/js/profileControl.js
@@ -8,7 +8,7 @@ const profileImgs = [
   "https://www.naruto-guides.com/wp-content/uploads/2019/05/naruto-uzumaki.jpg",
   "https://cbsnews1.cbsistatic.com/hub/i/r/2015/06/09/56a5c461-f85d-4622-b021-f6f20a3618bc/thumbnail/1200x630/cbd370c5256b07b5f42fef1db1f841b7/ppgkeyartpr2016.jpg"
 ]
-
+let c = 1
 if (
   window.location.pathname === "/profile"
 ) {
