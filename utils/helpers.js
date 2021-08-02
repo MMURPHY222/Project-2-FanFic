fb6d18f16a09e79a72c42ddda1a7d02e3f1e6d9a
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-02 08:17:04 -0400

    Added neccessary query selectors to helpersjs

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,4 @@
 const enterButton = document.querySelector("enter-button");
+const chatInput = document.querySelector("chat-input");
+const sessionInput = document.querySelector("session-input");
+const chatForm = document.querySelector("chat-form");
