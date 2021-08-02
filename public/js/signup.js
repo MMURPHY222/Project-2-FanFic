0cdd98a9970e1758138a204000b3841de95d6228
Tyler Starling <tyler.a.starling@gmail.com>
2021-08-02 13:31:51 -0400

    added comments to js files in public folder making fetch requests

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,17 +1,18 @@
+// creates a new user
 const signupFormHandler = async (event) => {
     event.preventDefault();
-  
+    // take info from user input fields and trim white space
     const user_name = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-  
+    // if all fields filled, fetch api route to post to server
     if (user_name && email && password) {
       const response = await fetch('/api/users/signup', {
         method: 'POST',
         body: JSON.stringify({ user_name, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
+      // if successful, redirect to profile
       if (response.ok) {
         document.location.replace('/profile');
       } else {
@@ -20,7 +21,7 @@ const signupFormHandler = async (event) => {
     }
   };
 
-  
+// event listener on submit 
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
--- public/js/writeStory.js
+++ public/js/writeStory.js
@@ -1,10 +1,11 @@
+// create a new story
 const newStoryHandler = async (event) => {
     event.preventDefault();
-  
+    // gather appropriate content from user input
     const title = document.querySelector('#story-title').value.trim();
     const summary = document.querySelector('#story-summary').value.trim();
     const story_content = document.querySelector('#story-content').value.trim();
-  
+    // fetch call to post route
     if (title && summary && story_content) {
       const response = await fetch(`/api/newstory`, {
         method: 'POST',
@@ -15,13 +16,14 @@ const newStoryHandler = async (event) => {
       });
   
       if (response.ok) {
+        // if successful, redirect to profile page
         document.location.replace('/profile');
       } else {
         alert('Failed to create project');
       }
     }
   };
-
+// event listener on submit
 document
   .querySelector('.story-form')
   .addEventListener('submit', newStoryHandler);
