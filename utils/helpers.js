ab222cde57f5ce9ab4c5c94e758eed004a91a211
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-02 08:18:16 -0400

    Added submit event listener boilerplate

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,3 +2,7 @@ const enterButton = document.querySelector("enter-button");
 const chatInput = document.querySelector("chat-input");
 const sessionInput = document.querySelector("session-input");
 const chatForm = document.querySelector("chat-form");
+
+chatForm.addEventListener("submit", event => {
+    event.preventDefault();
+})
